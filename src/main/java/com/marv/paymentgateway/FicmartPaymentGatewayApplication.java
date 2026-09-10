988de3ef7a3ce0e613906d4a948ce42f8f54fdd1
MarvsConcept <marvade8@gmail.com>
2026-09-10 08:37:37 +0100

    chore: add PostgreSQL container for payment gateway

---
+++ src/main/java/com/marv/paymentgateway/FicmartPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.marv.paymentgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicmartPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicmartPaymentGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marv/paymentgateway/FicmartPaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marv.paymentgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicmartPaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
